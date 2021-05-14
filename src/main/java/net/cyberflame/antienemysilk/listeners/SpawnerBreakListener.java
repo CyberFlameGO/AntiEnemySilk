207302b95b64d45450204e09cd1b943dad99c9a0
CyberFlame <cyberflameu@gmail.com>
2021-05-14 18:32:56 +1200

    Update SpawnerBreakListener.java

--- src/main/java/net/cyberflame/antienemysilk/listeners/SpawnerBreakListener.java
+++ src/main/java/net/cyberflame/antienemysilk/listeners/SpawnerBreakListener.java
@@ -1,27 +1,34 @@
 package net.cyberflame.antienemysilk.listeners;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 
 import com.massivecraft.factions.Board;
 import com.massivecraft.factions.FLocation;
 import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.FPlayers;
 import com.massivecraft.factions.Faction;
-import de.dustplanet.silkspawners.events.SilkSpawnersSpawnerBreakEvent;
 
 
 public class SpawnerBreakListener implements Listener
 {
 	@EventHandler
-	public void onSpawnerBreak(SilkSpawnersSpawnerBreakEvent e)
+	public void onSpawnerBreak(BlockBreakEvent e) 
 	{
+		@SuppressWarnings("unused")
+		Faction faction = null;
 		Player p = e.getPlayer();
 		FPlayer fp = FPlayers.getInstance().getByPlayer(p);
+		FLocation fLoc = new FLocation(p.getLocation());
+		faction = Board.getInstance().getFactionAt(fLoc);
+		Block b = e.getBlock();
+		
 		fp.checkIfNearbyEnemies();
-		p.sendMessage(p.getName() + fp.getName() + fp + p + fp.isInOwnTerritory() + fp.hasEnemiesNearby());
-		if (fp.isInOwnTerritory() && fp.hasEnemiesNearby())
+		if (fp.isInOwnTerritory() & b.getType() == Material.MOB_SPAWNER && fp.hasEnemiesNearby())
 			e.setCancelled(true);
 	}
 }
