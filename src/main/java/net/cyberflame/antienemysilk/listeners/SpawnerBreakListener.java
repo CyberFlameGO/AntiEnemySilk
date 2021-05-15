062c57eda06589549140c953998ed390710aa9eb
CyberFlame <cyberflameu@gmail.com>
2021-05-16 11:55:11 +1200

    Trying something else

--- src/main/java/net/cyberflame/antienemysilk/listeners/SpawnerBreakListener.java
+++ src/main/java/net/cyberflame/antienemysilk/listeners/SpawnerBreakListener.java
@@ -2,33 +2,46 @@ package net.cyberflame.antienemysilk.listeners;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
-import com.massivecraft.factions.Board;
-import com.massivecraft.factions.FLocation;
 import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.FPlayers;
-import com.massivecraft.factions.Faction;
+import com.massivecraft.factions.struct.Relation;
 
 
 public class SpawnerBreakListener implements Listener
 {
+	public boolean enemiesNearby = false;
 	@EventHandler
 	public void onSpawnerBreak(BlockBreakEvent e) 
 	{
-		@SuppressWarnings("unused")
-		Faction faction = null;
 		Player p = e.getPlayer();
 		FPlayer fp = FPlayers.getInstance().getByPlayer(p);
-		FLocation fLoc = new FLocation(p.getLocation());
-		faction = Board.getInstance().getFactionAt(fLoc);
 		Block b = e.getBlock();
 		
-		fp.checkIfNearbyEnemies();
-		if (fp.isInOwnTerritory() & b.getType() == Material.MOB_SPAWNER && fp.hasEnemiesNearby())
-			e.setCancelled(true);
+		if (p == null) return;
+	    if (p.hasPermission("aes.bypassnearbyenemycheck")) return;
+	    int radius = 50;
+	    for (@SuppressWarnings("unused") Entity ent : p.getNearbyEntities(radius, 255, radius)) 
+	    {
+	        if (e instanceof Player) 
+	        {
+	            Player eplayer = (((Player) e).getPlayer());
+	            if (eplayer == null) continue;
+	            if (eplayer.hasMetadata("NPC")) continue;
+	            FPlayer efplayer = FPlayers.getInstance().getByPlayer(eplayer);
+	            if (efplayer == null) continue;
+	            if (!p.canSee(eplayer) || efplayer.isVanished()) continue;
+	            if (fp.getRelationTo(efplayer).equals(Relation.ENEMY) && !efplayer.isStealthEnabled()) 
+	            	enemiesNearby = true;
+	    		if (fp.isInOwnTerritory() & b.getType() == Material.MOB_SPAWNER && enemiesNearby == true)
+	    			e.setCancelled(true);
+	        }
+	    }
 	}
 }
+
